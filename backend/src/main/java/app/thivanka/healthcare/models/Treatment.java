955835594947f50f3bf1fef58841d628b3b78616
dilanka-240 <dilankadineth@gmail.com>
2026-07-20 22:28:03 +0530

    backend: fix dto do not send patient number when add tratment

--- backend/src/main/java/app/thivanka/healthcare/models/Treatment.java
+++ backend/src/main/java/app/thivanka/healthcare/models/Treatment.java
@@ -7,7 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -20,7 +20,7 @@ public class Treatment {
 	private Long treat_id;
 
 	@ManyToOne
-	@JoinTable(name = "no")
+	@JoinColumn(name = "no")
 	private Patient patient;
 
 	@Column(nullable = false)
