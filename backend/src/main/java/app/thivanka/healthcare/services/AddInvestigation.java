040c8a1979f3632dfc9bf2930c0a88c0027e6b03
dilanka-240 <dilankadineth@gmail.com>
2026-08-04 21:20:20 +0530

    backend: add delete investigation and refactor investigation service

--- backend/src/main/java/app/thivanka/healthcare/services/AddInvestigation.java
+++
@@ -1,8 +0,0 @@
-package app.thivanka.healthcare.services;
-
-import app.thivanka.healthcare.dto.InvestigationDTO;
-
-public interface AddInvestigation{
-	
-	InvestigationDTO addInvestigation(InvestigationDTO investigationDto);
-}
--- backend/src/main/java/app/thivanka/healthcare/services/InvestigationServiceImpl.java
+++ backend/src/main/java/app/thivanka/healthcare/services/InvestigationServiceImpl.java
@@ -7,21 +7,35 @@ import org.springframework.transaction.annotation.Transactional;
 import app.thivanka.healthcare.dto.InvestigationDTO;
 import app.thivanka.healthcare.dto.UpdateInvestigationDTO;
 import app.thivanka.healthcare.mappers.InvestigationMapper;
-import app.thivanka.healthcare.models.Investigation;
-import app.thivanka.healthcare.models.Patient;
+import app.thivanka.healthcare.mappers.UpdateInvestigationMapper;
 import app.thivanka.healthcare.repositories.InvestigationRepository;
 import app.thivanka.healthcare.repositories.PatientRepository;
-import app.thivanka.healthcare.mappers.UpdateInvestigationMapper;
+import app.thivanka.healthcare.models.Investigation;
+import app.thivanka.healthcare.models.Patient;
 
 @Service
-public class UpdateInvestigationImpl implements UpdateInvestigation{
-	
-	@Autowired
-	InvestigationRepository investigationRepository;
+public class InvestigationServiceImpl implements InvestigationService{
 	
 	@Autowired
 	PatientRepository patientRepository;
 	
+	@Autowired
+	InvestigationRepository investigationRepository;
+
+	// Create new investigation
+	public InvestigationDTO addInvestigation(InvestigationDTO investigationDto) {
+		Long no = investigationDto.getNo();
+		Patient patient = patientRepository.findByNo(no)
+				.orElseThrow(() -> new IllegalArgumentException("Patient does not exits"));
+		
+		Investigation investigation = InvestigationMapper.toEntity(investigationDto);
+		investigation.setPatient(patient);
+		investigationRepository.save(investigation);
+		
+		return InvestigationMapper.toDto(investigation);
+	}
+	
+	// Update existing investigation
 	@Transactional
 	public UpdateInvestigationDTO updateInvestigation(UpdateInvestigationDTO updateInvestigationDto) {
 
@@ -35,5 +49,15 @@ public class UpdateInvestigationImpl implements UpdateInvestigation{
 		investigationRepository.save(investigation);
 		
 		return UpdateInvestigationMapper.toDto(investigation);
+	}
+
+	// Delete existing investigation
+	public void deleteInvestigation(Long invId) {
+		
+		if(!investigationRepository.existsById(invId)) {
+			throw new IllegalArgumentException("Investigation not exists");
+		}
+		
+		investigationRepository.deleteById(invId);
 	}
 }
