e412e19b44a4851bd88044074b5f09447d1448b2
dilanka-240 <dilankadineth@gmail.com>
2026-08-15 23:10:23 +0530

    backend: Add jwt authentication with access token and refresh token

--- backend/src/main/java/app/thivanka/healthcare/dto/LoginDTO.java
+++ backend/src/main/java/app/thivanka/healthcare/dto/LoginDTO.java
@@ -1,37 +1,54 @@
 package app.thivanka.healthcare.dto;
 
+public class LoginDTO {
 
-public class LoginDTO{
-	
-	private Long id;
-	private String name;
-	private String password;
-	
-	//setters
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	//getters
-	
-	public String getName() {
-		return name;
-	}
-	
-	public Long getId() {
-		return id;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	
+  private Long id;
+  private String name;
+  private String password;
+  private String accessToken;
+  private String refreshToken;
+
+  // setters
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public void setAccessToken(String accessToken) {
+    this.accessToken = accessToken;
+  }
+
+  public void setRefreshToken(String refreshToken) {
+    this.refreshToken = refreshToken;
+  }
+
+  // getters
+
+  public String getName() {
+    return name;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getAccessToken() {
+    return accessToken;
+  }
+
+  public String getRefreshTokne() {
+    return refreshToken;
+  }
+
 }
--- backend/src/main/java/app/thivanka/healthcare/services/AuthServiceImpl.java
+++ backend/src/main/java/app/thivanka/healthcare/services/AuthServiceImpl.java
@@ -1,27 +1,34 @@
 package app.thivanka.healthcare.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import app.thivanka.healthcare.dto.LoginDTO;
 import app.thivanka.healthcare.models.Doctor;
 import app.thivanka.healthcare.repositories.DoctorRepository;
+import app.thivanka.healthcare.security.JwtUtil;
 
-public class AuthServiceImpl implements AuthService{
-	
-	@Autowired
-	DoctorRepository doctorRepository;
-	
-	public LoginDTO login(LoginDTO loginDto){
-		
-		Doctor doctor = doctorRepository.findById(loginDto.getId())
-				.orElseThrow(() -> new IllegalArgumentException("Account not exists"));
-		
-		if(loginDto.getPassword() == doctor.getPassword()) {
-			return loginDto;
-		}else {
-			throw new Exception("Password is incorrect");
-		}
-	}
+@Service
+public class AuthServiceImpl implements AuthService {
+
+  @Autowired
+  DoctorRepository doctorRepository;
+
+  public LoginDTO login(LoginDTO loginDto) {
+
+    Doctor doctor = doctorRepository.findById(loginDto.getId())
+        .orElseThrow(() -> new IllegalArgumentException("Account not exists"));
+
+    if (loginDto.getPassword().equals(doctor.getPassword())) {
+      String accessToken = JwtUtil.generateAccessToken(doctor.getName());
+      String refreshToken = JwtUtil.generateRefreshToken(doctor.getName());
+      loginDto.setAccessToken(accessToken);
+      loginDto.setRefreshToken(refreshToken);
+
+      return loginDto;
+    } else {
+      throw new IllegalArgumentException("Password invalid");
+    }
+
+  }
 }
-
-
