19c1ce1540e06c6e5b6d789a21c08cd145a23df9
dilanka-240 <dilankadineth@gmail.com>
2026-08-23 21:33:57 +0530

    frontend: Add UpdateExamination.tsx

--- frontend/app/api/ExaminationApi.tsx
+++ frontend/app/api/ExaminationApi.tsx
@@ -1,15 +1,25 @@
-const API_URL = "http://10.0.2.2:8080/patient/{no}/exam";
+const BASE_URL = "http://10.0.2.2:8080/patient";
 
 export const addExamination = async (exam: any) => {
-  const res = await fetch(API_URL, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+  const res = await fetch(`${BASE_URL}/{no}/exam`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify(exam),
   });
-
   if (!res.ok) {
-    throw new Error('Faild to add examiantion');
+    throw new Error("Failed to add examination");
   }
   return res.json();
 };
 
+export const updateExamination = async (exam: any) => {
+  const res = await fetch(`${BASE_URL}/{no}/exam/{examId}/update`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(exam),
+  });
+  if (!res.ok) {
+    throw new Error("Failed to update examination");
+  }
+  return res.json();
+};
